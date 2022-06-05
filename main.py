b02c837308ea21a7b28bd3d2489dab94955a4666
Valentin <valbrqd@gmail.com>
2022-06-05 20:05:51 +0200

    essaie de creation de cube = echec

--- main.py
+++ main.py
@@ -49,11 +49,46 @@ def main():
     o4.transformation.translation.z = 25
     o3 = Object3D(VAO, m.get_nb_triangles(), program3d_id, texture, Transformation3D())
     viewer.add_object(o3)
+
 #===================== Création du batiment du musée ===================================
+    program = glutils.create_program_from_file('shadermusee.vert','shadermusee.frag')
+    GL.glUseProgram(program)
+    sommetsmusee = np.array(((-1.0,-1.0,-1.0),(-1.0,-1.0, 1.0),(-1.0, 1.0, 1.0), 
+    (1.0, 1.0,-1.0),(-1.0,-1.0,-1.0),(-1.0, 1.0,-1.0), 
+    (1.0,-1.0, 1.0),(-1.0,-1.0,-1.0),(1.0,-1.0,-1.0),
+    (1.0, 1.0,-1.0),(1.0,-1.0,-1.0),(-1.0,-1.0,-1.0),
+    (-1.0,-1.0,-1.0),(-1.0, 1.0, 1.0),(-1.0, 1.0,-1.0),
+    (1.0,-1.0, 1.0),(-1.0,-1.0, 1.0),(-1.0,-1.0,-1.0),
+    (-1.0, 1.0, 1.0),(-1.0,-1.0, 1.0),(1.0,-1.0, 1.0),
+    (1.0, 1.0, 1.0),(1.0,-1.0,-1.0),(1.0, 1.0,-1.0),
+    (1.0,-1.0,-1.0),(1.0, 1.0, 1.0),(1.0,-1.0, 1.0),
+    (1.0, 1.0, 1.0),(1.0, 1.0,-1.0),(-1.0, 1.0,-1.0),
+    (1.0, 1.0, 1.0),(-1.0, 1.0,-1.0),(-1.0, 1.0, 1.0),
+    (1.0, 1.0, 1.0),(-1.0, 1.0, 1.0),(1.0,-1.0, 1.0)),np.float32)
+
+    # attribution d'une liste d'etat (1 indique la cr ´ eation d'une seule liste) ´
+    vao = GL.glGenVertexArrays(1)
+    # affectation de la liste d'etat courante ´
+    GL.glBindVertexArray(vao)
+    # attribution d’un buffer de donnees (1 indique la cr ´ eation d’un seul buffer) ´
+    vbo = GL.glGenBuffers(1)
+    # affectation du buffer courant
+    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, vbo)
+
+    GL.glBufferData(GL.GL_ARRAY_BUFFER, sommetsmusee, GL.GL_STATIC_DRAW)
+
+    # Les deux commandes suivantes sont stockees dans l' ´ etat du vao courant ´
+    # Active l'utilisation des donnees de positions ´
+    # (le 0 correspond a la location dans le vertex shader) `
+    GL.glEnableVertexAttribArray(0)
+    # Indique comment le buffer courant (dernier vbo "binde") ´
+    # est utilise pour les positions des sommets ´
+    GL.glVertexAttribPointer(0, 3, GL.GL_FLOAT, GL.GL_FALSE, 0, None)
 
 
-
+    GL.glDrawArrays(GL.GL_TRIANGLES, 0, 36)
 #=======================================================================================
+
     vao = Text.initalize_geometry()
     texture = glutils.load_texture('fontB.jpg')
     o = Text('', np.array([-0.8, 0.3], np.float32), np.array([0.8, 0.8], np.float32), vao, 2, programGUI_id, texture)
