5363feed3b26397c1bbce06e3d477b9125bc78f1
Rodolphe Lajugie <ggrodizz@cpe.fr>
2022-06-09 16:14:40 +0200

    pas de conflis svp

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main():
     tr21.translation.y = 3.5
     tr21.translation.z = -10
     tr21.rotation_center.z = 0.2
-    texture = glutils.load_texture('ressources/textures/grass.jpg')
+    texture = glutils.load_texture('ressources/textures/or.jpg')
     o21 = Object3D(m21.load_to_gpu(), m21.get_nb_triangles(), program3d_id, texture ,tr21)
     #o21.transformation.translation.y = 2
     viewer.add_object(o21)
@@ -49,7 +49,7 @@ def main():
     t0, t1, t2, t3 = [0, 0], [1, 0], [1, 1], [0, 1]
     m.vertices = np.array([[p0 + n + c + t0], [p1 + n + c + t1], [p2 + n + c + t2], [p3 + n + c + t3]], np.float32)
     m.faces = np.array([[0, 1, 2], [0, 2, 3]], np.uint32)
-    texture = glutils.load_texture('ressources/textures/blanc.jpg')
+    texture = glutils.load_texture('ressources/textures/sol.jpg')
     VAO = m.load_to_gpu()
     tr6 = Transformation3D()
     tr6.translation.y = 28
@@ -62,7 +62,7 @@ def main():
     t05, t15, t25, t35 = [0, 0], [1, 0], [1, 1], [0, 1]
     m5.vertices = np.array([[p05 + n5 + c5 + t05], [p15 + n5 + c5 + t15], [p25 + n5 + c5 + t25], [p35 + n5 + c5 + t35]], np.float32)
     m5.faces = np.array([[0, 1, 2], [0, 2, 3]], np.uint32)
-    texture = glutils.load_texture('ressources/textures/grass.jpg')
+    texture = glutils.load_texture('ressources/textures/sol.jpg')
     VAO = m5.load_to_gpu()
     o1 = Object3D(VAO, m.get_nb_triangles(), program3d_id, texture, Transformation3D())
     viewer.add_object(o1)
@@ -73,13 +73,26 @@ def main():
     viewer.add_object(o)
     o = Text('', np.array([-0.5, -0.2], np.float32), np.array([0.5, 0.3], np.float32), vao, 2, programGUI_id, texture)
     viewer.add_object(o)
+    
+    #===================================== Stand ===================================
+    ms = Mesh.load_obj('ressources/objets/pedestal.obj')
+    ms.normalize()
+    ms.apply_matrix(pyrr.matrix44.create_from_scale([1,1,1,1]))
+    trs = Transformation3D()
+    trs.translation.y = 0
+    trs.translation.z = -10
+    trs.rotation_center.z = 0.2
+    texture = glutils.load_texture('ressources/textures/BaseColor.png')
+    os = Object3D(ms.load_to_gpu(), m21.get_nb_triangles(), program3d_id, texture ,trs)
+    os.transformation.translation.y = 2
+    viewer.add_object(os)
 #================================ Cube mur ========================================
 
     #===== contour =====#
     m2 = Mesh.load_obj('ressources/objets/cube.obj')
     m2.normalize()
-    m2.apply_matrix(pyrr.matrix44.create_from_scale([1, 20, 1, 1]))
-    texture2 = glutils.load_texture('ressources/textures/mur.jpg')
+    m2.apply_matrix(pyrr.matrix44.create_from_scale([1, 1, 1, 1]))
+    texture2 = glutils.load_texture('ressources/textures/noir.jpg')
     vao2 = m2.load_to_gpu()
     for val in range(30):
         tr2 = Transformation3D()
@@ -114,7 +127,7 @@ def main():
         viewer.add_object(o5)
     
     
-    #===== un seul bloc par côté pour remplir jusqu'au plafond (donc 4 blocs) =====#
+    #===== Bloc haut mur (donc 4 blocs) =====#
     m3 = Mesh.load_obj('ressources/objets/cube.obj')
     m3.normalize()
     m3.apply_matrix(pyrr.matrix44.create_from_scale([0.9, 30, 30, 1]))
@@ -159,41 +172,6 @@ def main():
     tr9.translation.y = +1
     o9 = Object3D(vao6, m6.get_nb_triangles(), program3d_id, texture, tr9)
     viewer.add_object(o9)
-    
-#==================================================================================
-    
-    
-    
-    
-    
-    
-    
-    #création mur
-    # mur2 = Mesh()
-    # p01, p11, p21, p31 = [-25, 0, -25], [25, 0, -25], [-25, 25, -25], [25, 25, -25]
-    # n1, c1 = [0, 0, 1], [1, 1, 1]
-    # t01, t11, t21, t31 = [0, 0], [1, 0], [1, 1], [0, 1]
-    # mur2.vertices = np.array([[p01 + n1 + c1 + t01], [p11 + n1 + c1 + t11], [p21 + n1 + c1 + t21], [p31 + n1 + c1 + t31]], np.float32)
-    # mur2.faces = np.array([[0, 1, 2], [1, 2, 3]], np.uint32)
-    # texture = glutils.load_texture('grass.jpg')
-    # o2 = Object3D(mur2.load_to_gpu(), mur2.get_nb_triangles(), program3d_id, texture, Transformation3D())
-    # viewer.add_object(o2)
-    
-    # mur3 = Mesh()
-    # p02, p12, p22, p32 = [25, 0, -25], [25, 0, -5], [25, 25, -25], [25, 25, 25]
-    # n2, c2 = [1, 0, 0], [1, 1, 1]
-    # t02, t12, t22, t32 = [0, 0], [1, 0], [1, 1], [0, 1]
-    # mur3.vertices = np.array([[p02 + n2 + c2 + t02], [p12 + n2 + c2 + t12], [p22 + n2 + c2 + t22], [p32 + n2 + c2 + t32]], np.float32)
-    # mur3.faces = np.array([[0, 1, 2], [1, 2, 3]], np.uint32)
-    # texture = glutils.load_texture('grass.jpg')
-    # o3 = Object3D(mur3.load_to_gpu(), mur3.get_nb_triangles(), program3d_id, texture, Transformation3D())
-    # viewer.add_object(o3)
-    
-
-#===================== Création du batiment du musée ===================================
-
-    
-#=======================================================================================
 
     
 
